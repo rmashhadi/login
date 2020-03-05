ab00dc0efc1dfab7c22890a1d4c190951bdaff4c
rmashhadi <rme1383@gmail.com>
2020-03-05 06:08:17 +0330

    Updates

---
+++ precache-manifest.f8fd5d4875445f39e7c1994e21c716d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c892200807267376d7a9bf4403e6937f",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "23c16969325cf55cf049",
+    "url": "/login/static/css/main.0976a079.chunk.css"
+  },
+  {
+    "revision": "e96825a91764e5ed8f88",
+    "url": "/login/static/js/2.7127df0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/login/static/js/2.7127df0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23c16969325cf55cf049",
+    "url": "/login/static/js/main.d4019fad.chunk.js"
+  },
+  {
+    "revision": "dd4cd9a45387c535063d",
+    "url": "/login/static/js/runtime-main.4789183d.js"
+  },
+  {
+    "revision": "626be8b3c3039d48d99e128d2a16f049",
+    "url": "/login/static/media/background.626be8b3.png"
+  },
+  {
+    "revision": "372ac3f6809ac74e4cec27d1ec59eb47",
+    "url": "/login/static/media/userlock.372ac3f6.png"
+  }
+]);
